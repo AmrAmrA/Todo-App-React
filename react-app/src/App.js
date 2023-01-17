e22dc41984c4389e3225a210380b30c39637491f
Abd El Fattah Amr <amrelfattah.dev@gmail.com>
2023-01-17 01:53:29 +0100

    get input values

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { useState } from 'react';
 import './App.css';
 import bgImgDesktopLight from './bg-desktop-light.jpg';
 import moon from './icon-moon.svg';
 
 function App() {
+  const [text, setText] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (e) => {
+    e.preventDefault();
+    setTodos([...todos, text]);
+    setText('');
+    console.log(todos);
+  };
   return (
     <div className="App">
       <header className="App-header">
@@ -15,18 +25,20 @@ function App() {
           </div>
           <label htmlFor="todo__action"></label>
               <input type="text" id="todo__action" name="todo__action" className='todo__action' required
-              minLength="4" maxLength="8" size="10" placeholder='Create a new todo...'></input>
+              minLength="2"  size="10" placeholder='Create a new todo...'  
+              onChange={(e) => setText(e.target.value) }
+              onClick={addTodo}
+              ></input>
               <div className="tasks__list">
                 <div className="task__header">
                   <div className="task__wrapper">
                     <ul className="task__list">
-                      <li className="task__item"> Task list is empty !</li>
-                      <li className="task__item"> </li>
-                      <li className="task__item"> </li>
-                      <li className="task__item"> </li>
+                      <li className="task__item"> Tasks list is empty !</li>
                       <li className="task__item last__task"> <p className='items'><span className='items__number'>0</span> item left</p>  
                       <div className="centered__options">
-                      <p className='all'>All</p> <p className='active'>Active</p> <p className='completed'>Completed</p>
+                      <p className='all firstCenter'>All</p> 
+                      <p className='active'>Active</p> 
+                      <p className='completed'>Completed</p>
                       </div> 
                         <p className='clearCompleted'>Clear Completed</p></li>
                     </ul>
